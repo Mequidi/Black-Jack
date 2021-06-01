938e826117cf22cd405a25af6e66cccbc9b0ba65
Mequidi <nikhilsejwal234@gmail.com>
2021-06-01 16:13:52 +0530

    adde player info

--- main.js
+++ main.js
@@ -1,55 +1,96 @@
-let firstCard = 7;
-let secondCard = 10;
-let sum = firstCard + secondCard;
+let player = {
+    name : "Nikhil",
+    chips : "150"
+}
 
+isblackjack = false
 isAlive = false;
 isPlaying = false;
+cards = []
 message = "";
+errorMessage = "";
+
 let messageEl = document.getElementById("message-el");
 let cardsEl = document.getElementById("cards-el");
 let sumEl = document.getElementById("sum-el");
+let errorEl = document.getElementById("error-el");
+let playerEl = document.getElementById("player-el");
+
+function randomCard()
+{
+    let randomNumber=Math.floor((Math.random()*13)+1);
+    if(randomNumber === 1)
+        randomNumber = 11;
+    else if(randomNumber >10)
+        randomNumber = 10;
+    return randomNumber;
+}
 
 function startGame()
 {
-    isAlive = true;
-    isPlaying = true;
-    let cards = [firstCard,secondCard];
-    sum = firstCard + secondCard;
-    renderGame();
+    errorEl.textContent = "";
+    if(!isPlaying)
+    {
+        isAlive = true;
+        isPlaying = true;
+        let firstCard = randomCard();
+        let secondCard = randomCard();
+        cards = [firstCard,secondCard];
+        sum = firstCard + secondCard;
+        renderGame();   
+    }
+    else    
+    {
+        errorMessage = "Game has already started !";
+        errorEl.textContent = errorMessage;
+    }
 }
 
 function renderGame()
 {
     cardsEl.textContent = "Cards :";
+    sumEl.textContent = "Sum :"
+    playerEl.textContent = player.name + " $" + player.chips;
 
     for(let i = 0 ; i < cards.length ; i++)
     {
         cardsEl.textContent += cards[i] + " ";
-        sum 
     }
+    sumEl.textContent += sum;
     
     if(sum<=20)
     {
         message = "Would you like to draw a card ?"
     }
-    else if(sum===21)
+    else {if(sum===21)
     {
         message = "You got blackjack, you won!";
+        isblackjack = true;
+        isPlaying = false;
     }
     else 
     {
         message = "Im sorry you lost!";
+        isAlive = false;
+        isPlaying = false;
+    }
     }
     messageEl.textContent = message;
 }
+
 function newCard()
 {
-    let thirdCard = 10;
-    cards.push=thirdCard;
-    sum += thirdCard;
-    renderGame();
-
-
-
-
+    errorEl.textContent = "";
+    if(!isblackjack && isAlive)
+    {
+        let thirdCard = randomCard();
+        cards.push(thirdCard);
+        sum += thirdCard;
+        renderGame();
+    }
+    else
+    {
+        errorMessage = "You cant draw more cards , the game is over!";
+        errorEl.textContent = errorMessage;
+    }
 }
